cdec4b8ea66df3760dddfe294f1dfbe292b2c8f7
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2022-07-05 13:18:10 +0300

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -7,17 +7,15 @@ module.exports = {
     utilisation: '{prefix}help',
 };
 
-message.channel.send('
-    `play` Bir parça oynatır.
-    `queue` Sıraya bir parça ekler.
-    `pause` Oynatılan parçayı durdurur.
-    `resume` Durdurulan parçayı devam ettirir.
-    `skip` Sonraki parçayı çalar.
-    `back` Önceki parçayı çalar.
-    `volume` Ses düzeyini ayarlar.
-    `save` Parçayı kayıt eder.
-    `stop` Parçayı durdurur.
-    `search` Youtube araması yapar.
-    `nowplaying` Çalmakta olan parçayı gösterir.
+message.channel.send('`play` Bir parça oynatır.n\`queue` Sıraya bir parça ekler.n\
+    `pause` Oynatılan parçayı durdurur.n\
+    `resume` Durdurulan parçayı devam ettirir.n\
+    `skip` Sonraki parçayı çalar.n\
+    `back` Önceki parçayı çalar.n\
+    `volume` Ses düzeyini ayarlar.n\
+    `save` Parçayı kayıt eder.n\
+    `stop` Parçayı durdurur.n\
+    `search` Youtube araması yapar.n\
+    `nowplaying` Çalmakta olan parçayı gösterir.n\
 ');
  
