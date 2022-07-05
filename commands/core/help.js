ea8f195bfa5f727f6b13ba68a81eca81590f8daa
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2022-07-05 12:35:20 +0300

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -12,17 +12,17 @@ module.exports = {
         embed.setColor('RED');
 
         embed.setDescription('[CenterQuickMusic](https://www.youtube.com/channel/UCq7NsIPj_7DGwiKzeimi_mA)');
-        embed.addField('`play` Bir parça oynatır.');
-        embed.addField('`queue` Sıraya bir parça ekler.');
-        embed.addField('`pause` Oynatılan parçayı durdurur.');
-        embed.addField('`resume` Durdurulan parçayı devam ettirir.');
-        embed.addField('`skip` Sonraki parçayı çalar.');
-        embed.addField('`back` Önceki parçayı çalar.');
-        embed.addField('`volume` Ses düzeyini ayarlar.');
-        embed.addField('`save` Parçayı kayıt eder.');
-        embed.addField('`stop`Parçayı durdurur');
-        embed.addField('`search` Youtube araması yapar.');
-        embed.addField('`nowplaying` Çalmakta olan parçayı gösterir.')
+        embed.addField(`play Bir parça oynatır.`);
+        embed.addField(`queue Sıraya bir parça ekler.`);
+        embed.addField(`pause Oynatılan parçayı durdurur.`);
+        embed.addField(`resume Durdurulan parçayı devam ettirir.`);
+        embed.addField(`skip Sonraki parçayı çalar.`);
+        embed.addField(`back Önceki parçayı çalar.`);
+        embed.addField(`volume Ses düzeyini ayarlar.`);
+        embed.addField(`save Parçayı kayıt eder.`);
+        embed.addField(`stop Parçayı durdurur.`);
+        embed.addField(`search Youtube araması yapar.`);
+        embed.addField(`nowplaying Çalmakta olan parçayı gösterir.`)
 
         embed.setFooter(`${message.author.username}` , message.author.avatarURL({ dynamic: true }));
 
