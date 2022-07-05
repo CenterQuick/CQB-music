db2f36e371aa82b816a691430677e8a44b827600
CenterQuick <75475710+CenterQuick@users.noreply.github.com>
2022-07-05 13:01:58 +0300

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -6,26 +6,26 @@ module.exports = {
     showHelp: false,
     utilisation: '{prefix}help',
 
-    execute(client, message, args) {
-        const embed = new MessageEmbed();
 
-        embed.setColor('RED');
+    const embed = new MessageEmbed();
 
-        embed.setDescription('[CenterQuickMusic](https://www.youtube.com/channel/UCq7NsIPj_7DGwiKzeimi_mA)');
-        embed.addField('play Bir parça oynatır.');
-        embed.addField('queue Sıraya bir parça ekler.');
-        embed.addField('pause Oynatılan parçayı durdurur.');
-        embed.addField('resume Durdurulan parçayı devam ettirir.');
-        embed.addField('skip Sonraki parçayı çalar.');
-        embed.addField('back Önceki parçayı çalar.');
-        embed.addField('volume Ses düzeyini ayarlar.');
-        embed.addField('save Parçayı kayıt eder.');
-        embed.addField('stop Parçayı durdurur.');
-        embed.addField('search Youtube araması yapar.');
-        embed.addField('nowplaying Çalmakta olan parçayı gösterir.')
+    embed.setColor('RED');
 
-        embed.setFooter(`${message.author.username}` , message.author.avatarURL({ dynamic: true }));
+    embed.setDescription('[CenterQuickMusic](https://www.youtube.com/channel/UCq7NsIPj_7DGwiKzeimi_mA)');
+    embed.addField('play Bir parça oynatır.');
+    embed.addField('queue Sıraya bir parça ekler.');
+    embed.addField('pause Oynatılan parçayı durdurur.');
+    embed.addField('resume Durdurulan parçayı devam ettirir.');
+    embed.addField('skip Sonraki parçayı çalar.');
+    embed.addField('back Önceki parçayı çalar.');
+    embed.addField('volume Ses düzeyini ayarlar.');
+    embed.addField('save Parçayı kayıt eder.');
+    embed.addField('stop Parçayı durdurur.');
+    embed.addField('search Youtube araması yapar.');
+    embed.addField('nowplaying Çalmakta olan parçayı gösterir.')
 
-        message.channel.send({ embeds: [embed] });
-    },
+    embed.setFooter(`${message.author.username}` , message.author.avatarURL({ dynamic: true }));
+
+    message.channel.send({ embeds: [embed] });
+  
 };
